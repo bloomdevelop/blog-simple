38423237990e347a6159e23afc94f1036c4fee09
bloomdevelop <bloomdevelop@proton.me>
2024-10-29 12:29:19 -0400

    Initial Commit /w Template

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Bloom's Simple Blog";
+export const SITE_DESCRIPTION = 'This is my blog where I write current project status and other random thoughts.';
 
 // Remember to set your site URL in astro.config.mjs
