7266799f080b31a5986218f38386778ba6c30c66
bloomdevelop <bloomdevelop@proton.me>
2024-10-29 12:39:01 -0400

    chore: remove /

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://bloomdevelop.github.io/',
+	site: 'https://bloomdevelop.github.io',
 	base: 'blog-simple',
 	integrations: [mdx(), sitemap()],
 });
