590ade243e2f7772f594d58de1e9a59d43fc95a1
bloomdevelop <bloomdevelop@proton.me>
2024-10-29 13:02:32 -0400

    chore: use vercel instead

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://bloomdevelop.github.io',
-	base: 'blog-simple',
-	integrations: [mdx(), sitemap()],
+  site: 'https://bloomdevelop.github.io',
+  base: 'blog-simple',
+  integrations: [mdx(), sitemap()],
+  output: 'server',
+  adapter: vercel(),
 });
